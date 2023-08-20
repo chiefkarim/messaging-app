a9659836a327f6245b588ed83774b8e0587ce15d
chiefkarim <krimotop2016@gmail.com>
2023-08-20 10:42:04 +0100

    made the basic structure for the app with js array data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+//make a note found route
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" ,messages:messages});
 });
+router.get("/new", function (req, res, next) {
+  res.render('new',{title:'new'});
+});
+router.post('/new',function(req,res,next){
+  console.log(req.body)
+  messages.unshift({text:req.body.message,user:req.body.userName,added: new Date()})
+  res.redirect('/');
+
+})
+
+
 
 module.exports = router;
